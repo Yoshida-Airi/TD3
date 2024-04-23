de8505615fc154a1d7b85b08e1722eb7540801e4
k022g0004@g.neec.ac.jp <k022g0004@g.neec.ac.jp>
2024-04-23 14:48:11 +0900

    シーン遷移ボタンを左クリックに統一

--- Boss.cpp
+++ Boss.cpp
@@ -215,12 +215,15 @@ void Boss::Direction(float speed) {
 	//目標角度の算出
 	angle_ = std::atan2(speed_.x, speed_.z);
 
-	if (enemyPos.y > 0.0f)//落下
+	if (enemyPos.y >= 0.0f)//落下
 	{
 		speed_.x = 0.0f;
 		speed_.y = -0.05f;
 		speed_.z = 0.0f;
 	}
+	
+
+
 	model_->worldTransform_->rotation_.y = LerpShortAngle(model_->worldTransform_->rotation_.y, angle_, 0.1f);
 
 }
