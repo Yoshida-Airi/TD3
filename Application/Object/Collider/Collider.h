da46c37ee9968482b506025c5ffd23f442aeafb6
MasamiTuzukibashi <k022g0080@g.neec.ac.jp>
2024-05-21 15:32:12 +0900

    obb処理移動中

--- Application/Object/Collider/Collider.h
+++ Application/Object/Collider/Collider.h
@@ -14,6 +14,23 @@ public:
 	void UpdateWorldTransform();
 	void Draw( Camera* camera);
 
+	struct Sphere {
+		Vector3 center;
+		float radius;
+	};
+
+	struct AABB {
+		Vector3 min;
+		Vector3 max;
+	};
+
+	struct OBB {
+		Vector3 center;
+		Vector3 orientations[3];
+		Vector3 size;
+	};
+	OBB GetOBB()const { return obb_; }
+
 	//ワールド座標を取得
 	virtual Vector3 GetWorldPosition() = 0;
 
@@ -25,6 +42,8 @@ public:
 
 	//半径を設定
 	void SetRadius(Vector3 radius) { radius_ = radius; };
+	void SetOBB(OBB obb) { obb_ = obb; }
+
 
 	//衝突属性(自分)を取得
 	uint32_t GetCollisionAttribute() const { return collisionAttribute_; };
@@ -52,6 +71,7 @@ private:
 	//衝突半径
 	Vector3 radius_ = { 1.0f,1.0f,1.0f };
 	uint32_t typeID_ = 0u;
+	OBB obb_ = { {1.0f,1.0f,1.0f},{1.0f,1.0f,1.0f} };
 
 
 
